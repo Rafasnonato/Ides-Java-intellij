db2174b787dd5cdb6183986427cde7160bc7b29b
Rafasnonato <rafael.designer.int@gmail.com>
2022-05-17 18:58:41 -0300

    First commit

---
+++ src/Br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package Br.com.dio;
+
+import Br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+     /*   int a = 5 , b = 3;
+                  System.out.println("Hello world! " + (a+b));*/
+
+    }
+
+}
